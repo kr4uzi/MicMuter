c29b6f59e605f3d6bc01230a7a0fccdbc7480232
Markus Kraus <kr4uzi@gmail.com>
2020-05-02 21:54:57 +0200

    Initial Commit

---
+++ MicMuter/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MicMuter
+//
+//  Created by Markus Kraus on 02.05.20.
+//  Copyright © 2020 Markus Kraus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
